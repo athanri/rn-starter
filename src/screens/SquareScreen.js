ee1b110bf2060981ee4d945c44a6f39965fe9a3f
athanri <piercejord@gmail.com>
2023-01-26 13:23:56 +0000

    PJ - 26-01-2023 13:23

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,16 +1,55 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import ColorCounter from '../components/ColorCounter';
+import React, { useReducer } from "react";
+import { StyleSheet, View } from "react-native";
+import ColorCounter from "../components/ColorCounter";
+
+const COLOR_INCREMENT = 15;
+
+const reducer = (state, action) => {
+    // state === {red: number, green: number, blue: number };
+    // action === colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15
+    switch(action.colorToChange) {
+        case 'red':
+            return { ...state, red: state.red + action.amount };
+        case 'green':
+            return { ...state, green: state.green + action.amount };
+        case 'blue':
+            return { ...state, blue: state.blue + action.amount };
+        default:
+            return state;
+    }
+};
 
 const SquareScreen = () => {
-    return (
-        <View>
-           <ColorCounter color='Red' /> 
-           <ColorCounter color='Green' /> 
-           <ColorCounter color='Blue' /> 
-        </View>
-    )
-}
+
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0})
+
+  return (
+    <View>
+      <ColorCounter
+        onIncrease={() => dispatch({colorToChange : 'red', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({colorToChange : 'red', amount: -1 * COLOR_INCREMENT})}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => dispatch({colorToChange : 'green', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({colorToChange : 'green', amount: -1 * COLOR_INCREMENT})}
+        color="Green"
+      />
+      <ColorCounter
+        onIncrease={() => dispatch({colorToChange : 'blue', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({colorToChange : 'blue', amount: -1 * COLOR_INCREMENT})}
+        color="Blue"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`,
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
